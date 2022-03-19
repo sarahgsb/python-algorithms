f2448615fa93685789544caf798f12a6442967ab
Sarah <sarahgsb@hotmail.com>
2022-03-19 18:33:08 -0300

    Requisito 1 - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    total = 0
+    try:
+        for check_in, check_out in permanence_period:
+            if check_in <= target_time <= check_out:
+                total += 1
+        return total
+    except TypeError:
+        return None
