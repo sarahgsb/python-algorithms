010ce1358cb664105ead576cb0b9ba25c18d4689
Sarah <sarahgsb@hotmail.com>
2022-03-19 21:38:21 -0300

    Requisito 3 - is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,26 @@
+def selection_sort(string):
+    word = list(string)
+
+    for i in range(len(word)):
+        min = i
+
+        for j in range(i + 1, len(word)):
+            if word[j] < word[min]:
+                min = j
+
+        word[min], word[i] = word[i], word[min]
+
+    return word
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    if first_string == "" or second_string == "":
+        return False
+    if selection_sort(first_string.lower()) == selection_sort(
+        second_string.lower()
+    ):
+        return True
+
+    return False
